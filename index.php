9482594a0ac6a7af0e29a59b0e50b2db3ee32e8a
joaov-vasconcelos <81269019+joaov-vasconcelos@users.noreply.github.com>
2023-02-15 21:34:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,11 @@
-<html>
-<body>
-
 <?php
-    print("hello world");
-    echo "<h1>Esse é um comando interpretado </h1>";
-
-    $valor = "teste de variavel";
-    $valor2 = 2;
-    $valor3 = 3.2;
-
-    echo $valor . "</br>";
-    echo $valor2 . "</br>";
-    echo $valor3 . "</br>";
-
-    echo gettype($valor). "</br>";
-    echo gettype($valor2). "</br>";
-    echo gettype($valor3). "</br>";
-
-    $misto = ["estrutura",0.2,1];
-
-    echo $misto[0]. "</br>";
-
-    var_dump($misto). "</br>";
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
 ?>
-
-</body>
-</html>
+Something is wrong with the XAMPP installation :-(
